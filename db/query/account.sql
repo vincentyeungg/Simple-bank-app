f5a6e228755b3aa5d0dd9dbd56d1f73eb44c782a
vincentyeungg <vincentyeung0809@gmail.com>
2022-07-25 00:07:14 -0400

    added update account balance, transfer logic and unit tests

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,7 +1,7 @@
 -- name: CreateAccount :one
 INSERT INTO accounts (
-  owner, 
-  balance, 
+  owner,
+  balance,
   currency
 ) VALUES (
   $1, $2, $3
@@ -10,8 +10,14 @@ RETURNING *;
 
 -- name: GetAccount :one
 SELECT * FROM accounts
-WHERE id = $1 
+WHERE id = $1
 LIMIT 1;
+
+-- name: GetAccountForUpdate :one
+SELECT * FROM accounts
+WHERE id = $1
+LIMIT 1
+FOR NO KEY UPDATE;
 
 -- name: ListAccounts :many
 SELECT * FROM accounts
@@ -23,6 +29,12 @@ OFFSET $2;
 UPDATE accounts
 SET balance = $2
 WHERE id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+SET balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteAccount :exec
